ad5a04e722383088b227f3453ab3007b72d396ab
Suleiman_Kuliev <getto_tawers@mail.ru>
2023-05-07 23:03:02 +0400

    Создание верстки карты

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/style.css'
+
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3VsZWltYW4tMjAwNSIsImEiOiJjbGg5MGl2NnYwMnYyM3BsdGNmaGM5bjhyIn0.8jzysqf1GcWc4-yZzRmxXA';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: [53, 50], // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
+
