d1282b7d7b41931747e541fb7f6c5ad5d5622303
Suleiman_Kuliev <getto_tawers@mail.ru>
2023-05-18 20:18:52 +0400

    Закончил работать с геокодером и картой

---
+++ environment.js
@@ -0,0 +1,3 @@
+export const mapboxApiKey =
+  'pk.eyJ1Ijoic3VsZWltYW4tMjAwNSIsImEiOiJjbGg5MGl2NnYwMnYyM3BsdGNmaGM5bjhyIn0.8jzysqf1GcWc4-yZzRmxXA';
+export const weatherApiKey = '';
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './styles/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css';
 
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3VsZWltYW4tMjAwNSIsImEiOiJjbGg5MGl2NnYwMnYyM3BsdGNmaGM5bjhyIn0.8jzysqf1GcWc4-yZzRmxXA';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { mapboxApiKey } from '../environment';
+
+mapboxgl.accessToken = mapboxApiKey;
+
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [53, 50], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  center: [53, 50], // starting position [lng, lat]
+  zoom: 9, // starting zoom
 });
 
+const geocoder = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  mapboxgl,
+});
+
+// document.querySelector('.mapboxgl-ctrl-geocoder--input').className = 'form-control';
+// document.querySelector('.mapboxgl-ctrl-geocoder--icon-search').remove();
+// map.addControl(geocoder);
+geocoder.addTo('#geocoder-container'); // прочитать addTo
+geocoder.on('result', (results) => {
+  map.flyTo({
+    center: results.result.center,
+  });
+});
