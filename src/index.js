4418c7fef4079abb4ea1a1e1f8cfe6561ef3f2f7
Suleiman_Kuliev <getto_tawers@mail.ru>
2023-06-23 22:23:27 +0400

    Спор в верстке (1 версия), перехожу к просто ограничению в блоке

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import './styles/style.css';
 import { initMap } from './modules/map/map';
 import { initBackgroundImg } from './modules/background/background';
 
+const refreshBg = document.querySelector('.refresh-bg');
+refreshBg.style.background = 'url("assets/icons/Refresh BG.png")';
+
 initBackgroundImg();
 initMap();
